a9feba30b170711a01e61fe4e4469bfeef0c50e1
keithhauzh <keithhau.thegreat@gmail.com>
2025-09-06 17:06:16 +0800

    extracted string resources

--- app/src/main/java/com/keith/word_app_cs2/MainActivity.kt
+++ app/src/main/java/com/keith/word_app_cs2/MainActivity.kt
@@ -1,27 +1,30 @@
 package com.keith.word_app_cs2
 
 import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.activity.addCallback
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.commit
 import androidx.navigation.NavController
+import androidx.navigation.NavHost
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.keith.word_app_cs2.databinding.ActivityMainBinding
+import com.keith.word_app_cs2.ui.fragments.home.HomeFragment
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var navController: NavController
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        enableEdgeToEdge()
 
-        val navHost = supportFragmentManager
-            .findFragmentById(
-            R.id.navHostFragment
-        ) as NavHostFragment
-        navController = navHost.findNavController()
-
-        findViewById<BottomNavigationView>(R.id.nav_view)
-            .setupWithNavController(navController)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
