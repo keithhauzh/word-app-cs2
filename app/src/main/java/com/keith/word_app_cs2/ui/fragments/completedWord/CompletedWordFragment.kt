da787b1209547b3d480a95fc9a2da20e85fe7888
keithhauzh <keithhau.thegreat@gmail.com>
2025-09-07 15:52:52 +0800

    extracted string resources

--- app/src/main/java/com/keith/word_app_cs2/ui/fragments/completedWord/CompletedWordFragment.kt
+++ app/src/main/java/com/keith/word_app_cs2/ui/fragments/completedWord/CompletedWordFragment.kt
@@ -1,7 +1,6 @@
 package com.keith.word_app_cs2.ui.fragments.completedWord
 
 import android.os.Bundle
-import android.provider.UserDictionary
 import android.view.LayoutInflater
 import androidx.fragment.app.Fragment
 import android.view.View
@@ -9,12 +8,14 @@ import android.view.ViewGroup
 import androidx.fragment.app.setFragmentResultListener
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.keith.word_app_cs2.databinding.FragmentCompletedWordBinding
 import com.keith.word_app_cs2.ui.adapter.WordsAdapter
-import com.keith.word_app_cs2.ui.fragments.home.HomeFragmentDirections
 import kotlinx.coroutines.launch
+import com.keith.word_app_cs2.R
 
 class CompletedWordFragment : Fragment() {
     private lateinit var binding: FragmentCompletedWordBinding
@@ -35,20 +36,33 @@ class CompletedWordFragment : Fragment() {
     }
 
     override fun onViewCreated(view:View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val navHostFragment = requireActivity().supportFragmentManager
+            .findFragmentById(R.id.navHostFragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
         adapter = WordsAdapter(emptyList()){
             val action = CompletedWordFragmentDirections.actionCompletedWordFragmentToWordDetailsFragment(
                 wordId = it.id!!,
             )
             findNavController().navigate(action)
         }
-        super.onViewCreated(view, savedInstanceState)
-        setFragmentResultListener("manage_word") {_, _ ->
+
+        binding.navView.setupWithNavController(navController)
+        binding.rvWords.layoutManager = LinearLayoutManager(requireContext())
+        binding.rvWords.adapter = adapter
+
+        setFragmentResultListener("manage_word") { _, _ ->
             viewModel.getWords()
         }
+
         lifecycleScope.launch {
-            viewModel.words.collect {
-                adapter.setWords(it)
+            viewModel.words.collect { words ->
+                adapter.setWords(words)
+                binding.llEmpty.visibility = if(words.isEmpty()) View.VISIBLE else View.GONE
             }
         }
+
+        viewModel.getWords()
     }
 }
--- app/src/main/java/com/keith/word_app_cs2/ui/fragments/home/HomeFragment.kt
+++ app/src/main/java/com/keith/word_app_cs2/ui/fragments/home/HomeFragment.kt
@@ -8,12 +8,10 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.setFragmentResultListener
 import androidx.lifecycle.lifecycleScope
-import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.keith.word_app_cs2.R
 import com.keith.word_app_cs2.ui.adapter.WordsAdapter
 import com.keith.word_app_cs2.databinding.FragmentHomeBinding
