ec6584cd5aa1984ebf97b05b53c7d9d531ce1452
kei <keithhau.thegreat@gmail.com>
2025-09-10 15:08:23 +0800

    started on MyDatabase, WordsDao, WordsRepo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     id("androidx.navigation.safeargs.kotlin")
+    kotlin("kapt")
 }
 
 android {
@@ -38,6 +39,12 @@ android {
     buildFeatures{
         viewBinding = true
     }
+
+    kapt {
+        arguments {
+            arg("room.schemaLocation", "$projectDir/schemas")
+        }
+    }
 }
 
 dependencies {
@@ -60,7 +67,7 @@ dependencies {
     implementation(libs.androidx.navigation.ui.ktx)
     implementation(libs.androidx.fragment.ktx)
 
-//    implementation("androidx.navigation:navigation-fragment-ktx:<version>")
-//    implementation("androidx.navigation:navigation-ui-ktx:<version>")
-//    implementation("androidx.fragment:fragment-ktx:<version>")
+    implementation("androidx.room:room-runtime:2.6.1")
+    implementation("androidx.room:room-ktx:2.6.1")
+    kapt("androidx.room:room-compiler:2.6.1")
 }
